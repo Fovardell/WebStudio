0614b83509f6114dc12aab7ca911d256b6dda729
Fovardell <Flatroniym@gmail.com>
2024-05-28 21:56:41 +0000

    Deploying to gh-pages from @ Fovardell/WebStudio@b6e0f90a6bdc26366c6f67224f94cfd53459da2a 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import{m as o,o as e}from"./assets/modal-b42a3676.js";const t=document.querySelector("[data-modal-open]"),n=document.querySelector(".button-hero"),c=document.querySelector(".mobile-contacts-js");o();n.addEventListener("click",e);t.addEventListener("click",e);c.addEventListener("click",e);
+//# sourceMappingURL=commonHelpers.js.map
---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import{m as o,o as e}from"./assets/modal-b42a3676.js";o();const n=document.querySelector("[data-modal-open]"),t=document.querySelector(".mobile-contacts-js");n.addEventListener("click",e);t.addEventListener("click",e);
+//# sourceMappingURL=commonHelpers2.js.map
